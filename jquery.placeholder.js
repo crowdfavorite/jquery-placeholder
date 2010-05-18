4fab85324730d7ec3ae10af303f802cb3ef29932
gbrander <gbrander@546ca710-db30-0410-8887-d9dd8e66aff0>
2010-05-18 23:15:53 +0000

    Terseness/consistency tweaks.
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/jquery-plugins/placeholder/trunk@24471 546ca710-db30-0410-8887-d9dd8e66aff0

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -3,14 +3,14 @@
  * Crowd Favorite
  * @requires jQuery v1.2 or above
  *
- * Version: 1.0
+ * Version: 1.0.1
  * Patches the HTML5 placeholder atttribute functionality for browsers that don't support it
  */
 ;(function($) {
 	$.fn.placeholder = function(settings) {
 		// Merge default options and user options
 		var opts = $.extend({}, $.fn.placeholder.settings, settings);
-		
+
 		/* Are we using the placholder attribute?
 		 * Does the browser support placeholders?
 		 * Should we run if it does?
@@ -19,7 +19,7 @@
 		if (opts.attribute == 'placeholder' && opts.disableIfSupported == true && 'placeholder' in document.createElement('input')) {
 			return null;
 		};
-		
+
 		// Run placholders
 		this.each(function() {
 			var _this = $(this);
@@ -34,7 +34,7 @@
 		});
 		clearPlaceholdersOnSubmit(opts);
 	};
-	
+
 	/**
 	 * Plugin settings defaults
 	 * Set in separate object so they are public
@@ -44,7 +44,7 @@
 		attribute: 'placeholder',
 		disableIfSupported: true
 	};
-	
+
 	/**
 	 * Call this to enable standard-style HTML5 placeholders globally
 	 */
@@ -56,7 +56,6 @@
 
 	function prepPlaceholder(el, opts) {
 		var c = opts.classname;
-		
 		if(el.val() == '' || el.val() == el.attr(opts.attribute)) {
 			el.addClass(c);
 			if(el.val() == '') {
@@ -68,13 +67,12 @@
 	};
 	function togglePlaceholder(el, opts) {
 		// Check if the input already has a value...
-		if((el.val() != '') && (el.val() != el.attr(opts.attribute))) {
+		if(el.val() && el.val() != el.attr(opts.attribute)) {
 			return false;
 		};
-		
 		if(el.val() == el.attr(opts.attribute)) {
 			el.attr('value', '');
-		} else if(el.val() == '') {
+		} else if(!el.val()) {
 			el.attr('value', el.attr(opts.attribute));
 		};
 		el.toggleClass(opts.classname);
@@ -87,9 +85,9 @@
 	clearPlaceholders = function(form, opts) {
 		$(form).find('input').each(function(){
 			_this = $(this);
-			if(_this.attr('value') == _this.attr(opts.attribute)) {
+			if(_this.val() == _this.attr(opts.attribute)) {
 				_this.attr('value', '');
 			};
 		});
-	}
+	};
 })(jQuery);
